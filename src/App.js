031cdce29f6012d1944c7cb05c8f8725524782b4
irwinpratajaya <irwinpratajaya@gmail.com>
2017-03-27 19:05:06 +0700

    moving data to state and props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import './App.css'
@@ -21,34 +21,39 @@ import './App.css'
 // }
 
 // const data = [1, 2, 3]
-const data = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        {
+          title: 'React',
+          url: 'https://facebook.github.io/react/',
+          author: 'Jordan Walke',
+          num_comments: 3,
+          points: 4,
+          objectID: 0,
+        },
+        {
+          title: 'Redux',
+          url: 'https://github.com/reactjs/redux',
+          author: 'Dan Abramov, Andrew Clark',
+          num_comments: 2,
+          points: 5,
+          objectID: 1,
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <ul>
-        { data.map((item) => {
+        { this.state.data.map((item, index) => {
           return (
-            <li><a href={item.url} target="_blank">{item.title}</a> </li>
+            <li key={index}><a href={item.url} target="_blank">{item.title}</a> </li>
           )
         })
       }
