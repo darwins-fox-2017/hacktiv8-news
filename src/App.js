a9ba5211c01be9602f5edde90fd0c245b5843ea4
Eri Irawan <eryirawan91@gmail.com>
2017-03-28 14:48:17 +0700

    State and props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const data = [
   {
-    title:'React',
-    url:'https://facebook.github.io/react/',
-    author:'Jordan Walke',
-    num_comments:3,
-    points:4,
-    objectID:0
-  },
-  {
-    title:'Redux',
-    url:'https://github.com/reactjs/redux/',
-    author:'Dan Abramov, Andrew Clark',
-    num_comments:2,
-    points:5,
-    objectID:1
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
   }
 ]
 
 class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			data : data
+		}
+	}
   render() {
     return (
       <div className="App">
@@ -35,7 +40,7 @@ class App extends Component {
 
         <div className="example">
           <ul>
-            {data.map((item, index)=>{
+            {this.state.data.map((item, index)=>{
               return(
                 <li key={index}><a href={item.url} target="_blank">{item.title}</a></li>
               )
