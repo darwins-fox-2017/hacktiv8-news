0cb718b06da862e93158565a40281abbb4da764e
Bambang Handoko <email": null>
2017-03-29 11:17:50 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -3,26 +3,6 @@ import './App.css';
 import Search from './components/Search'
 import NewList from './components/NewList'
 
-
-// const data = [
-//   {
-//     title: 'React',
-//     url: 'https:/facebook.github.io/react',
-//     author: 'Jordan Walke',
-//     num_comments: 3,
-//     points: 4,
-//     objectID: 0,
-//   },
-//   {
-//     title: 'Redux',
-//     url: 'https://github.com/reactjs/redux',
-//     author: 'Dan Abramov, Andrew Clark',
-//     num_comments: 2,
-//     points: 5,
-//     objectID: 1,
-//   },
-// ]
-
 class App extends Component {
   constructor() {
     super()
